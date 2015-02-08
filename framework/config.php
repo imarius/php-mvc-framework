6ae3e73eb77c69c52e63132b5cf813b90aab2a6b
Marius Laurentiu Ilie <marius.ilie.outlook@outlook.com>
2015-02-08 20:33:53 +0200

    Added basic default routing logic.

--- framework/config.php
+++ framework/config.php
@@ -9,6 +9,42 @@
 
 	/*
 	* Default Route Values
+	* TODO: Think of a better place to have this.
 	*/
-	
+
+	$controller = "home";
+	$action = "index";
+	$query = null;
+
+	if(isset($_GET['load']))
+	{
+		$params = array();
+		$params = explode("/", $_GET['load']);
+
+		$controller = ucwords($params[0]);
+
+		if(isset($params[1]) && !empty(params[1]))
+		{
+			$action = $params[1];
+		}
+
+		if(isset($params[2]) && !empty($params[2]))
+		{
+			$query = $params[2];
+		}
+	}
+
+	$modelName = $controller;
+	$controller .= 'Controller';
+	$load = new $Controller($modelName, $action);
+
+	if(method_exists($load, $action))
+	{
+		$load->$action($query);
+	}
+	else 
+	{
+		//TODO: Do this more elegantly.
+		die('Invalid method name.');
+	}
 ?>
